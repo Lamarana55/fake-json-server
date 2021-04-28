9da294bb4b539af1cd4ce38ba48dc234a23ae4d7
MAMADOU LAMARANA DIALLO <lamarana55@gmail.com>
2021-04-28 13:05:55 +0000

    Initial commit

---
+++ db.js
@@ -0,0 +1,22 @@
+function generateProducts() {
+    let products = [];
+    for (let index = 1; index <= 100; index++) {
+      let name = faker.vehicle.vehicle();
+      let price = faker.random.number({min: 1, max: 5}) * 1000000
+      let quantity = faker.random.number({min: 1, max: 9}) * 100
+      let selected = faker.random.boolean()
+      let available = faker.random.boolean()
+  
+          products.push({
+              "id": index,
+              "name": name,
+              "price": price,
+              "quantity": quantity, 
+              "selected": selected,
+              "available": available
+          })
+      }
+      return {"products": products}
+      
+  }
+  module.exports = generateProducts
